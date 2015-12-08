6d9540b9f12fbcf09a0b87d91817b5c5afcaf47c
Vladirien <vladirien@gmail.com>
2015-12-08 15:42:29 +0100

    QueryNN

--- src/IrisDataSetAnalyser.java
+++ src/IrisDataSetAnalyser.java
@@ -3,10 +3,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 
 public class IrisDataSetAnalyser {
 
+		
 	public static ArrayList<Iris> ReadFile(String csvFile){
 		ArrayList<Iris> result = new ArrayList<Iris>();
 		BufferedReader br = null;
@@ -57,8 +65,60 @@ public class IrisDataSetAnalyser {
 	}
 	
 	public static String QueryKNN(Iris iris, ArrayList<Iris> learningset, int N){
+		HashMap<Iris,Double> distances = new HashMap<Iris,Double>();
+		ArrayList<Iris> results = new ArrayList<Iris>();
+		int i;
+		//get distances
+		for(i=0;i<learningset.size();i++){			
+			distances.put(learningset.get(i), iris.getDistance(learningset.get(i), 2));
+		}
+		//sort distances
+		while(!distances.isEmpty()){
+			Iterator<Entry<Iris, Double>> entries = distances.entrySet().iterator();
+			Iris maxKey = null;
+			double maxValue = 0;
+			while(entries.hasNext()){
+				Map.Entry<Iris,Double> entry = (Map.Entry<Iris,Double>) entries.next();
+			    Iris key = (Iris)entry.getKey();
+			    Double value = (Double)entry.getValue();
+			    if(maxKey == null || maxValue < value){
+			    	maxKey = key;
+			    	maxValue = value;			    	
+			    }
+			    
+			}
+			results.add(maxKey);
+			distances.remove(maxKey);			
+		}
+		HashMap<String,Double> neighbors = new HashMap<String,Double>();
 		
-		return "Iris Type Placeholder";
+		//get neighbors scores
+		for(i=0;i<results.size();i++){		
+			Iris r = results.get(i);
+			if(neighbors.containsKey(r.type)){
+				double d = neighbors.get(r.type);
+				d++;
+				System.out.println("Added neighbor "+ r.type);
+				neighbors.put(r.type, d);				
+			}else{
+				neighbors.put(r.type, 1.0);
+			}
+		}
+		
+		Iterator<Entry<String, Double>> entries = neighbors.entrySet().iterator();
+		String maxKey = null;
+		double maxValue = 0;
+		while(entries.hasNext()){
+			Map.Entry<String,Double> entry = (Map.Entry<String,Double>) entries.next();
+			String key = (String)entry.getKey();
+		    Double value = (Double)entry.getValue();
+		    if(maxKey == null || maxValue < value){
+		    	maxKey = key;
+		    	maxValue = value;			    	
+		    }
+		}
+		
+		return maxKey;
 	}
 	
 	public static void TestKNNModel(ArrayList<Iris> learningset,ArrayList<Iris> testset, int N){
@@ -70,7 +130,9 @@ public class IrisDataSetAnalyser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Iris> dataset=IrisDataSetAnalyser.ReadFile("./datasets/iris.data");
-		IrisDataSetAnalyser.TestDistance(new Iris(3,2,5,2,"Jeej"), dataset);
+		//IrisDataSetAnalyser.TestDistance(new Iris(3,2,5,2,"Jeej"), dataset);
+		
+		//System.out.println("Predicted :"+IrisDataSetAnalyser.QueryKNN(new Iris(3,2,5,2,"Jeej"), dataset, 3));
 	}
 
 }
